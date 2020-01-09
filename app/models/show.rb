a5a52cb4aa95e34149a4eaa7f4981b31d4ed2f13
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2020-01-09 05:20:37 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -15,4 +15,12 @@ class Show < ActiveRecord::Base
   def self.least_popular_show
     self.where("rating = ?", self.lowest_rating).first
   end
+  
+  def self.ratings_sum
+    self.sum(:rating)
+  end
+  
+  def self.popular_shows
+    self.where("rating > ?", 5)
+  end
 end
