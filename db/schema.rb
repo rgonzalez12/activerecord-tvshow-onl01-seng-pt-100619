c0aba86849b2f5c6a37980bf6d869ef78e3394b1
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2020-01-09 05:08:28 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2) do
+
+  create_table "shows", force: :cascade do |t|
+    t.string "name"
+    t.string "network"
+    t.string "day"
+    t.integer "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "season"
+  end
 
 end
